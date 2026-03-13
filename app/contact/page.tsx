c68cb383b6ad2949656f4105eb4ca2285efa544f
Sofiqul Islam <sofiqulislam@Sofiquls-Mac-mini.local>
2026-03-13 07:15:30 +0600

    Fix error from contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
-import IntroBG from "../assets/images/introduction-bg.jpg";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import Introduction from "../components/history/Introduction";
+import RabbisPeriodSection from "../components/history/RabbisPeriodSection";
 import LoadingEffect from "../components/LoadingEffect";
 import { gsap } from "../ui/plugins";
 
@@ -56,15 +55,9 @@ export default function Contact() {
         className="main opacity-0 relative overflow-hidden z-10"
       >
         <div className="h-screen w-screen flex items-center justify-center relative">
-          <Introduction
-            animated={isAllAnimationComplete}
-            bgImage={IntroBG}
-            extraClass={"panel-section min-w-screen w-screen"}
-            audioControl={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            panel={undefined}
-            data={[]}
+          <RabbisPeriodSection
+            animWidthText={0.1}
+            extraClass={"min-w-[90vw] w-[90vw] h-screen"}
           />
         </div>
         <div className="h-screen w-screen flex items-center justify-center"></div>
