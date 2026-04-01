51a1402f3c78fb497c7d0cbe1e73e99bbd463a4e
Sofiqul Islam <sofiqulislam@Sofiquls-Mac-mini.local>
2026-04-02 00:59:18 +0600

    Completing Communities Sheets page animation and Scorring

--- app/components/sheets/SheetContentSection.tsx
+++ app/components/sheets/SheetContentSection.tsx
@@ -1,6 +1,7 @@
 import SearchIcon from "@/app/assets/icons/SearchIcon";
 import GetHebrewYear from "@/app/ui/GetHebrewYear";
 import SheetContentItem from "@/app/ui/SheetContentItem";
+import { useEffect, useRef } from "react";
 import SimpleBar from "simplebar-react";
 
 interface ChildProps {
@@ -9,17 +10,27 @@ interface ChildProps {
 }
 
 export default function SheetContentSection(props: ChildProps) {
+  // Selector
+  const scrollbarRef = useRef<HTMLDivElement>(null);
   // Section Data
   const Years = ["תשפ״ו", "תשפ״ה", "תשפ״ד", "תשפ״ג"];
+  // Section Animation
+  useEffect(() => {
+    const selectYears = scrollbarRef.current?.querySelectorAll(".year-month");
+    if (selectYears) {
+      selectYears[0].querySelector(".months")?.classList.remove("hidden");
+      selectYears[0].querySelector(".months")?.classList.add("flex");
+    }
+  }, []);
   return (
     <section
       dir="rtl"
       className={`${props.extraClass} bg-black flex items-center justify-start relative z-20`}
     >
       <div className="sheet-wrapper w-full h-auto flex items-center gap-x-[5.8vw]">
-        <div className="sheet-sidebar w-54.5 h-full pl-10 will-change-transform">
-          <SimpleBar style={{ maxHeight: "80vh" }} autoHide={false}>
-            <div className="search-group relative mb-[3.6vh] -ml-5">
+        <div className="sheet-sidebar w-54.5 h-full will-change-transform overflow-hidden">
+          <div className="sheet-sidebar-wrapper">
+            <div className="search-group relative mb-[3.6vh]">
               <input
                 className="text-[24px] text-[#D1A941] placeholder:text-[#D1A941] leading-[1em] bg-white p-2.25 focus:outline-0 max-w-full pl-8"
                 type="text"
@@ -31,12 +42,20 @@ export default function SheetContentSection(props: ChildProps) {
                 <SearchIcon />
               </button>
             </div>
-            <div className="year-month-categories">
-              {Years.map((item, index) => {
-                return <GetHebrewYear key={index} year={item} />;
-              })}
+            <div ref={scrollbarRef} className="sheet-scrollbar-wrapper">
+              <SimpleBar
+                style={{ maxHeight: "60vh" }}
+                autoHide={false}
+                data-simplebar-direction="rtl"
+              >
+                <div className="year-month-categories pl-10">
+                  {Years.map((item, index) => {
+                    return <GetHebrewYear key={index} year={item} />;
+                  })}
+                </div>
+              </SimpleBar>
             </div>
-          </SimpleBar>
+          </div>
         </div>
         <div className="sheet-content flex items-center gap-x-[3.2vw] will-change-transform">
           <SheetContentItem />
